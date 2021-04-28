5df8d9597f7ec093715e384cc8cd9e01ac403241
evillt <ijoec123@gmail.com>
2021-04-28 21:55:56 +0800

    chore: rename files to avoid misleading.

--- vite.library.vue2.config.js
+++ vite.library.vue2.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import { buildOptions } from './vite.library.config'
 
-buildOptions.build.lib.entry = path.resolve(__dirname, 'src', 'VueCusdisV2')
+buildOptions.build.lib.entry = path.resolve(__dirname, 'src', 'VueCusdisVue2')
 buildOptions.build.lib.fileName = 'vue2'
 
 export default defineConfig(buildOptions)
